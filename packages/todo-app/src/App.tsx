753b1acbb92af2fc7bcfb76f53277c0a7021261c
Yunji Kim <normaltic@gmail.com>
2025-09-15 18:59:47 +0900

    Apply react-query devtools

--- packages/todo-app/src/App.tsx
+++ packages/todo-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./pages/Route";
 import ReactQueryProvider from "./contexts/ReactQueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <BrowserRouter>
       <ReactQueryProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
         <AppRoutes />
       </ReactQueryProvider>
     </BrowserRouter>
